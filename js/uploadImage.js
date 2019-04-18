bf76abf8c1e4c96a272542036c7ed934e45dda04
IMadiev <ilgiz.madiev@mail.ru>
2019-04-19 00:31:51 +0300

    Fix uploading File

--- js/uploadImage.js
+++ js/uploadImage.js
@@ -1,58 +1,41 @@
-let dropArea = document.getElementById('drop-area')
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false)
-})
-
-function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
-}
-;['dragenter', 'dragover'].forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight, false)
-})
-;['dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight, false)
-})
-function highlight(e) {
-    dropArea.classList.add('highlight')
-}
-function unhighlight(e) {
-    dropArea.classList.remove('highlight')
-}
-
-dropArea.addEventListener('drop', handleDrop, false)
-function handleDrop(e) {
-    let dt = e.dataTransfer
-    let files = dt.files
-    handleFiles(files)
-}
+let imgFile = null;
+let okButton = document.getElementsByClassName('accept-image')[0];
+okButton.addEventListener('click', acceptImage, false);
 
 function handleFiles(files) {
-    files = [...files]
-    files.forEach(uploadFile)
-    files.forEach(previewFile)
-}
-
-function uploadFile(file) {
-    let url = 'ВАШ URL ДЛЯ ЗАГРУЗКИ ФАЙЛОВ'
-    let formData = new FormData()
-    formData.append('file', file)
-    fetch(url, {
-        method: 'POST',
-        body: formData
-    })
-        .then(() => { /* Готово. Информируем пользователя */ })
-        .catch(() => { /* Ошибка. Информируем пользователя */ })
+    files = [...files];
+    files.forEach(previewFile);
+    imgFile=files[0]
 }
 
 function previewFile(file) {
-    let reader = new FileReader()
-    reader.readAsDataURL(file)
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onloadend = function() {
-        let img = document.createElement('img')
-        img.src = reader.result
-        img.className+='img-responsive'
-        document.getElementById('gallery').appendChild(img)
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.className+='img-responsive';
+        let gallery = document.getElementById('gallery');
+        while (gallery.firstChild){
+            gallery.removeChild(gallery.firstChild)
+        }
+        gallery.appendChild(img)
+    }
+}
+
+function acceptImage() {
+    document.getElementById('gallery').removeChild(document.getElementById('gallery').firstChild);
+    let reader = new FileReader();
+    reader.readAsDataURL(imgFile);
+    reader.onloadend = function() {
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.className+='img-responsive';
+        let imageWrapper = document.getElementById('image');
+        while (imageWrapper.firstChild){
+            imageWrapper.removeChild(imageWrapper.firstChild)
+        }
+        imageWrapper.appendChild(img)
     }
 }
 
